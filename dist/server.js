e4b7b74b70be76888803850debe51f25b091d301
Joao Oliveira <joao.oliveira7@br.ey.com>
2024-06-04 21:42:33 -0300

    Initial

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,19 @@
-"use strict";
 console.clear();
+export class Node {
+    value;
+    next;
+    previous;
+    constructor(value) {
+        this.value = value;
+        this.previous = null;
+        this.next = null;
+    }
+}
+export class DoublyLinkedList {
+    head;
+    tail;
+    length;
+    constructor() {
+        this.length = 0;
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
 console.clear();
+
+export class Node<T> {
+  public value: T;
+  public next: Node<T> | null;
+  public previous: Node<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.previous = null;
+    this.next = null;
+  }
+}
+
+export class DoublyLinkedList<T> {
+  private head: Node<T> | null;
+  private tail: Node<T> | null;
+  private length: number;
+
+  constructor() {
+    this.length = 0;
+  }
+}
