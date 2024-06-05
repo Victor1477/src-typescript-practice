4d7022bf0468de39916c8e27675e38b3f54fae65
Joao Oliveira <joao.oliveira7@br.ey.com>
2024-06-04 21:56:49 -0300

    push

--- dist/server.js
+++ dist/server.js
@@ -5,8 +5,6 @@ export class Node {
     previous;
     constructor(value) {
         this.value = value;
-        this.previous = null;
-        this.next = null;
     }
 }
 export class DoublyLinkedList {
@@ -16,4 +14,31 @@ export class DoublyLinkedList {
     constructor() {
         this.length = 0;
     }
+    push(value) {
+        const node = new Node(value);
+        if (this.length === 0) {
+            this.head = node;
+            this.tail = node;
+        }
+        else {
+            node.previous = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
+    forEach(callback) {
+        let currentItem = this.head;
+        while (currentItem) {
+            callback(currentItem.value);
+            currentItem = currentItem.next;
+        }
+    }
 }
+const list = new DoublyLinkedList();
+for (let i = 0; i <= 5; i++) {
+    list.push(i);
+}
+list.forEach((value) => {
+    console.log(value);
+});
--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,51 @@ console.clear();
 
 export class Node<T> {
   public value: T;
-  public next: Node<T> | null;
-  public previous: Node<T> | null;
+  public next: Node<T>;
+  public previous: Node<T>;
 
   constructor(value: T) {
     this.value = value;
-    this.previous = null;
-    this.next = null;
   }
 }
 
 export class DoublyLinkedList<T> {
-  private head: Node<T> | null;
-  private tail: Node<T> | null;
-  private length: number;
+  private head: Node<T>;
+  private tail: Node<T>;
+  public length: number;
 
   constructor() {
     this.length = 0;
   }
+
+  push(value: T) {
+    const node = new Node<T>(value);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.previous = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
+
+  forEach(callback: (value: T) => void) {
+    let currentItem = this.head;
+    while (currentItem) {
+      callback(currentItem.value);
+      currentItem = currentItem.next;
+    }
+  }
 }
+
+const list = new DoublyLinkedList<number>();
+
+for (let i = 0; i <= 5; i++) {
+  list.push(i);
+}
+
+list.forEach((value) => {
+  console.log(value);
+});
