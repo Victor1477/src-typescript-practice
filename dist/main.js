5ac85f1752bba4e1a557912a9a8429ab46718f8c
João Victor Silva De Oliveira <joao.soliveira@telefonicati.onmicrosoft.com>
2025-12-30 09:56:41 -0300

    Improvements

--- dist/main.js
+++ dist/main.js
@@ -6,9 +6,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const orders_json_1 = __importDefault(require("../orders.json"));
 console.clear();
-var fileContent = "";
+var Configuration;
+(function (Configuration) {
+    Configuration["ORDER_STATUS"] = "ERROR_RESERVE_DEVICE";
+    Configuration["ACTION_ID"] = "reserveStock";
+})(Configuration || (Configuration = {}));
+var fileContent = `import de.hybris.platform.core.model.order.OrderModel
+import de.hybris.platform.ordersplitting.model.ConsignmentModel
+import de.hybris.platform.ordersplitting.model.ConsignmentProcessModel
+import de.hybris.platform.processengine.BusinessProcessService
+import de.hybris.platform.servicelayer.model.ModelService
+
+BusinessProcessService businessProcessService = spring.getBean("businessProcessService");
+ModelService modelService = spring.getBean("modelService");
+
+String[] processCodes = [\n`;
 orders_json_1.default.forEach((order) => {
-    if (order.status === "ERROR_RESERVE_DEVICE")
-        fileContent += `"consignment-process_a${order.order}_${order.order}",\n`;
+    if (order.status === Configuration.ORDER_STATUS) {
+        if (orders_json_1.default[orders_json_1.default.length - 1] !== order) {
+            fileContent += `"consignment-process_a${order.order}_${order.order}",\n`;
+        }
+        else {
+            fileContent += `"consignment-process_a${order.order}_${order.order}"`;
+        }
+    }
 });
-fs_1.default.writeFileSync("orders.txt", fileContent);
+fileContent += `\n]
+
+for (String processCode : processCodes) {
+    ConsignmentProcessModel process = businessProcessService.getProcess(processCode)
+    if (process != null) {
+        println("Reprocessando: " + processCode)
+        businessProcessService.restartProcess(process, "${Configuration.ACTION_ID}");
+    } else {
+        println("Processo não encontrado: " + processCode);
+    }
+}`;
+fs_1.default.writeFileSync("reprocess-orders.groovy", fileContent);
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,42 @@ import orders from "../orders.json";
 
 console.clear();
 
-var fileContent = "";
+enum Configuration {
+  ORDER_STATUS = "ERROR_RESERVE_DEVICE",
+  ACTION_ID = "reserveStock",
+}
+
+var fileContent = `import de.hybris.platform.core.model.order.OrderModel
+import de.hybris.platform.ordersplitting.model.ConsignmentModel
+import de.hybris.platform.ordersplitting.model.ConsignmentProcessModel
+import de.hybris.platform.processengine.BusinessProcessService
+import de.hybris.platform.servicelayer.model.ModelService
+
+BusinessProcessService businessProcessService = spring.getBean("businessProcessService");
+ModelService modelService = spring.getBean("modelService");
+
+String[] processCodes = [\n`;
 
 orders.forEach((order) => {
-  if (order.status === "ERROR_RESERVE_DEVICE")
-    fileContent += `"consignment-process_a${order.order}_${order.order}",\n`;
+  if (order.status === Configuration.ORDER_STATUS) {
+    if (orders[orders.length - 1] !== order) {
+      fileContent += `"consignment-process_a${order.order}_${order.order}",\n`;
+    } else {
+      fileContent += `"consignment-process_a${order.order}_${order.order}"`;
+    }
+  }
 });
 
-fs.writeFileSync("orders.txt", fileContent);
+fileContent += `\n]
+
+for (String processCode : processCodes) {
+    ConsignmentProcessModel process = businessProcessService.getProcess(processCode)
+    if (process != null) {
+        println("Reprocessando: " + processCode)
+        businessProcessService.restartProcess(process, "${Configuration.ACTION_ID}");
+    } else {
+        println("Processo não encontrado: " + processCode);
+    }
+}`;
+
+fs.writeFileSync("reprocess-orders.groovy", fileContent);
